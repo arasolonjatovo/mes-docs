4f82cfd360bc549d4df50d91ee28cea77f04b6f6
Audrey Rasolonjatovo <rasolonjatovo.audrey@gmail.com>
2024-02-20 23:54:01 +0100

    ADD style

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-import App from './App';
+import './style/main.scss'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import App from './pages/App'
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
