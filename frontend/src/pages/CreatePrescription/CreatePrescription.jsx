05f8facffa37a56dba18288fc6ebebee367cbec0
Audrey Rasolonjatovo <rasolonjatovo.audrey@gmail.com>
2024-02-21 15:15:54 +0100

    ADD medication handling

--- frontend/src/pages/CreatePrescription/CreatePrescription.jsx
+++ frontend/src/pages/CreatePrescription/CreatePrescription.jsx
@@ -1,4 +1,6 @@
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
+
+import { IoMdTrash } from 'react-icons/io'
 
 import Button from '../../components/Button/Button'
 
@@ -23,6 +25,25 @@ export default function CreatePrescription() {
   const ageRef = useRef(null)
   const weightRef = useRef(null)
 
+  const dosageRef = useRef(null)
+  const medicationRef = useRef(null)
+  const instructionRef = useRef(null)
+
+  const [medications, setMedications] = useState([])
+
+  const handleMedication = (event) => {
+    event.preventDefault()
+    const newMedication = {
+      medication: medicationRef.current.value,
+      dosage: dosageRef.current.value,
+      instruction: instructionRef.current.value,
+    }
+    setMedications([...medications, newMedication])
+    medicationRef.current.value = null
+    dosageRef.current.value = null
+    instructionRef.current.value = null
+  }
+
   const handleSubmit = async (event) => {
     event.preventDefault()
     const formData = {
@@ -36,6 +57,7 @@ export default function CreatePrescription() {
       patientAge: ageRef.current.value,
       patientWeight: weightRef.current.value,
       generationDate: getTodaysDate(new Date()),
+      prescriptions: medications,
     }
 
     console.log(formData)
@@ -63,73 +85,121 @@ export default function CreatePrescription() {
     <div className="prescription">
       <h1>Nouvelle ordonnance</h1>
       <form>
-        <div className="prescription__doc">
-          <h2>Vos informations</h2>
-          <input
-            type="text"
-            placeholder="Votre nom"
-            id="name"
-            autoComplete="name"
-            ref={nameRef}
-          />
-          <input
-            type="text"
-            placeholder="Votre adresse"
-            id="address"
-            autoComplete="address"
-            ref={addressRef}
-          />
-          <input
-            type="text"
-            placeholder="Votre ville et code postal"
-            id="city"
-            ref={cityRef}
-          />
-          <input
-            type="text"
-            placeholder="Votre numéro de téléphone"
-            id="phone"
-            autoComplete="phone"
-            ref={phoneRef}
+        <div className="prescription__leftPart">
+          <div className="prescription__doc">
+            <h2>Vos informations</h2>
+            <input
+              type="text"
+              placeholder="Votre nom"
+              id="name"
+              autoComplete="name"
+              ref={nameRef}
+            />
+            <input
+              type="text"
+              placeholder="Votre adresse"
+              id="address"
+              autoComplete="address"
+              ref={addressRef}
+            />
+            <input
+              type="text"
+              placeholder="Votre ville et code postal"
+              id="city"
+              ref={cityRef}
+            />
+            <input
+              type="text"
+              placeholder="Votre numéro de téléphone"
+              id="phone"
+              autoComplete="phone"
+              ref={phoneRef}
+            />
+          </div>
+          <div className="prescription__patient">
+            <h2>Informations du patient</h2>
+            <select name="gender" id="gender-select" ref={genderRef}>
+              <option value="Madame">Madame</option>
+              <option value="Monsieur">Monsieur</option>
+            </select>
+            <input
+              type="text"
+              placeholder="Nom du patient"
+              id="patientName"
+              ref={patientSNameRef}
+            />
+            <input
+              type="text"
+              placeholder="Prénom du patient"
+              id="patientFName"
+              ref={patientFNameRef}
+            />
+            <input
+              type="number"
+              placeholder="Âge du patient"
+              id="patientAge"
+              ref={ageRef}
+              min={1}
+            />
+            <input
+              type="number"
+              placeholder="Poids du patient (en kg)"
+              id="patientWeight"
+              ref={weightRef}
+              min={1}
+            />
+          </div>
+        </div>
+        <div className="prescription__rightPart">
+          <div className="prescription__list">
+            <div className="prescription__medicationsList">
+              <h2>Votre prescription</h2>
+              <input
+                type="text"
+                placeholder="Nom du médicament"
+                id="medication-name"
+                ref={medicationRef}
+              />
+              <input
+                type="number"
+                placeholder="Dosage (en mg)"
+                min={1}
+                id="medication-dosage"
+                ref={dosageRef}
+              />
+              <input
+                type="text"
+                placeholder="Instructions"
+                id="medication-instruction"
+                ref={instructionRef}
+              />
+              <Button
+                label="Ajouter un médicament"
+                variant="secondary"
+                onClick={handleMedication}
+              />
+            </div>
+          </div>
+          <div className="prescription__resume">
+            <h2>Résumé</h2>
+            <ul>
+              {medications.map((medication, index) => (
+                <li key={index}>
+                  <p>
+                    {medication.medication} • {medication.dosage}mg •{' '}
+                    {medication.instruction} {index}
+                  </p>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <Button
+            type="submit"
+            onClick={handleSubmit}
+            variant="primary"
+            label="Générer"
           />
         </div>
-        <div className="prescription__patient">
-          <h2>Informations du patient</h2>
-          <select name="gender" id="gender-select" ref={genderRef}>
-            <option value="Madame">Madame</option>
-            <option value="Monsieur">Monsieur</option>
-          </select>
-          <input
-            type="text"
-            placeholder="Nom du patient"
-            id="patientName"
-            ref={patientSNameRef}
-          />
-          <input
-            type="text"
-            placeholder="Prénom du patient"
-            id="patientFName"
-            ref={patientFNameRef}
-          />
-          <input
-            type="number"
-            placeholder="Âge du patient"
-            id="patientAge"
-            ref={ageRef}
-          />
-          <input
-            type="number"
-            placeholder="Poids du patient (en kg)"
-            id="patientWeight"
-            ref={weightRef}
-          />
-        </div>
-        <Button
-          type="submit"
-          onClick={handleSubmit}
-          variant="primary"
-          label="Générer"
-        />
       </form>
     </div>
   )
